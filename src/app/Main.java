dd85a9a945da860a7c4d6c7f91ec1505e83b4664
DrPaul1982 <avtoklavir@ukr.net>
2024-09-25 14:00:15 +0300

    Add functional for converting miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,21 @@
 package src.app;
 
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static final double difKmToMl = 1.60934;
 
     public static void main(String[] args) {
         System.out.println("This app will help you to convert kilometers to miles and vice versa (miles to kilimeters).");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, input distance in miles: ");
+        double mls = scanner.nextDouble();
+        System.out.println("The distance " + mls + " miles in kilometers will be " + convMlsToKms(mls));
+    }
+
+    private static double convMlsToKms(double mls) {
+        return mls * difKmToMl;
     }
 }
