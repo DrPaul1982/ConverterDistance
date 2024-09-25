1a27b7f35052a096c1ea60ed2fd4b901e3424ad8
DrPaul1982 <avtoklavir@ukr.net>
2024-09-25 13:40:23 +0300

    Add project description

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package src.app;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This app will help you to convert kilometers to miles and vice versa (miles to kilimeters).");
+    }
+}
